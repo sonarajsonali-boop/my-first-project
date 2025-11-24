4b38dac05befb4515e7b99daf45bfbae31b1993e
sonarajsonali-boop <sonarajsonali@gmail.com>
2025-11-24 18:17:39 +0530

    employee data project

---
+++ employee_data_project.sql
@@ -0,0 +1,10 @@
+CREATE TABLE employee_data_ (
+    EmployeeID INT PRIMARY KEY,
+    Name VARCHAR(100),
+    Department VARCHAR(50),
+    Role VARCHAR(100),
+    JoiningDate DATE,
+    Salary DECIMAL(10,2),
+    PerformanceRating INT,
+    Attrition VARCHAR(3)
+); 
